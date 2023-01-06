8b5fece4a4ec8cafaa3b0fb9c8e69cd441328a26
Dinesh Deshmukh <deshmukhdinesh00@gmail.com>
2023-01-06 09:44:53 +0530

    Regisrty for all library services

---
+++ src/main/java/com/fis/microservices/LibraryRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.fis.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LibraryRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fis/microservices/LibraryRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
